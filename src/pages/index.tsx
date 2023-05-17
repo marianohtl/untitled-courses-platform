ca5bf78a9871cbfd91e5d16aae9b703aab00fc69
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2023-05-16 22:36:31 -0300

    System Design Added

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import style from "./style.module.scss";
+import React from "react";
+
+export default function Home() {
+	return (
+		<>
+			<h1 className={style["test-cool"]}>HELL</h1>
+		</>
+	);
+}
