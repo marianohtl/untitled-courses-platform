13a09bd8d2a3dbce29e96e891a94d09706deae8b
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2023-05-19 00:16:47 -0300

    Basic header structure added

---
+++ src/layouts/header/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+//import style from "./header.module.scss";
+
+export default function Header() {
+    return (
+        <div>
+            <div>
+                <div>
+                    <img src=""/>
+                </div>
+                <div>
+                    Search something ...
+                </div>
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Sign-in</a>
+                    </li>
+                    <li>
+                        <a href="#">Sign-up</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import Header from "@/layouts/header";
 import style from "./style.module.scss";
 import React from "react";
 
 export default function Home() {
 	return (
 		<>
+			<Header/>
 			<h1 className={style["test-cool"]}>HELL</h1>
 		</>
 	);
