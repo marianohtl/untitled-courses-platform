cebf302c3ea182fca64daf8a24a338956938007c
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2023-05-28 02:21:41 -0300

    Changes Header structure

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -2,32 +2,55 @@ import React from "react";
 import style from "./header.module.scss";
 import ContentSearch from "@/components/content-search";
 
+interface IHeaderButtonProps {
+    label: string,
+    path: string
+}
+function Button({label, path}: IHeaderButtonProps){
+    return (
+        <li className={style["button"]}>
+            <a href={path}>{label}</a>
+        </li>
+    );
+}
+
+function Buttons(){
+    return (
+        <nav>
+            <ul>
+                <Button path="#" label="Sign-in"/>
+                <Button path="#" label="Sign-up"/>
+            </ul>
+        </nav>
+    );
+}
+
+interface IHeaderLogoProps {
+    size: number
+}
+function Logo({size}: IHeaderLogoProps){
+    return(
+        <div>
+            <a href="/">
+                <img 
+                    src="/images/mafia_duck.png" 
+                    width={size}
+                    height={size}
+                    draggable={false}
+                />
+            </a>
+        </div>
+    );
+}
+
 export default function Header() {
     return (
         <header id={style["root"]}>
             <div>
-                <div>
-                    <a href="/">
-                        <img 
-                            src="/images/mafia_duck.png" 
-                            width={48}
-                            height={48}
-                            draggable={false}
-                        />
-                    </a>
-                </div>
+                <Logo size={48}/>
                 <ContentSearch placeholder="Search something..." /> 
             </div>
-            <nav>
-                <ul>
-                    <li className={style["button"]}>
-                        <a href="#">Sign-in</a>
-                    </li>
-                    <li className={style["button"]}>
-                        <a href="#">Sign-up</a>
-                    </li>
-                </ul>
-            </nav>
+            <Buttons />
         </header>
     );
 }
