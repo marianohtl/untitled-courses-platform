572994568e4e0e7c1b81de933d620b4a4461872c
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2023-05-28 02:44:18 -0300

    Add Home2 component (temporary name)

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home2(){
+    return (
+        <>
+            TODO: rename to Home and reference it in pages/index.tsx
+        </>
+    );
+}
