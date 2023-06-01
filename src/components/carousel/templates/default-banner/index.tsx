5bc0f60fa9c225faa78e2d6f5c763b2a99096235
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2023-06-01 12:49:03 -0300

    Adds params to default banner

--- src/components/carousel/templates/default-banner/index.tsx
+++ src/components/carousel/templates/default-banner/index.tsx
@@ -3,41 +3,57 @@ import React from "react";
 import style from "./default-banner.module.scss";
 import CarouselItem, { ICarouselItemProps } from "../../carousel-item";
 
-interface ICarouselDefaultBanner extends ICarouselItemProps{
-    image: string,
+interface ICarouselDefaultBannerButton {
+    url: string,
+    name: string
 }
 
-export default function CarouselDefaultBanner({ image, background }: ICarouselDefaultBanner){
+interface ICarouselDefaultBanner extends ICarouselItemProps {
+    image: string,
+    title: string,
+    textData: string[],
+    buttons?: ICarouselDefaultBannerButton[]
+}
+
+export default function CarouselDefaultBanner({
+    image, background,
+    title, textData,
+    buttons
+}: ICarouselDefaultBanner) {
     return (
         <CarouselItem background={background}>
             <div className={style["root"]}>
                 <div className={style["image-container"]}>
-                    <img src={image} draggable={false} width={640} height={480}/>
+                    <img src={image} draggable={false} width={640} height={480} />
                 </div>
                 <section className={style["text-container"]}>
-                    <h1>Lorem Ipsum!</h1>
+                    <h1>{title}</h1>
                     <div className={style["text"]}>
                         <div>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi expedita, quia iste impedit sit maiores ducimus hic! 
-                                Rem odio iusto ipsum soluta reiciendis, sint quibusdam laborum maxime sed non accusantium?
-                            </p>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                                Error delectus accusantium ut porro libero temporibus suscipit obcaecati molestias expedita repellendus sint, amet tenetur fugit facilis! 
-                                Soluta iure cum fugiat vitae?
-                            </p>
+                            {
+                                textData.map((text) => (
+                                    <p key={text.split(" ")[0]}>{text}</p>
+                                ))
+                            }
                         </div>
-                        <nav className={style["buttons"]}>
-                            <ul>
-                                <li>
-                                    <a href="#">Trilhas</a>
-                                </li>
-                                <li>
-                                    <a href="#">Cursos</a>
-                                </li>
-                            </ul>
-                        </nav>
+                        {
+                            buttons &&
+                            (
+                                <nav className={style["buttons"]}>
+                                    <ul>
+                                        {
+                                            buttons?.map(
+                                                button => (
+                                                    <li key={button.url}>
+                                                        <a href={button.url}>{button.name}</a>
+                                                    </li>
+                                                )
+                                            )
+                                        }
+                                    </ul>
+                                </nav>
+                            )
+                        }
                     </div>
                 </section>
             </div>
--- src/pages/components/banner/index.tsx
+++ src/pages/components/banner/index.tsx
@@ -12,6 +12,19 @@ export default function Banner(){
                 <CarouselDefaultBanner 
                     background="https://placehold.co/1920x750/FFFFFF/F6F6F6.png" 
                     image="https://placehold.co/640x480/000000/FFFFFF.png" 
+                    title="Lorem Ipsum!"
+                    textData={
+                        [
+                            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi expedita, quia iste impedit sit maiores ducimus hic! Rem odio iusto ipsum soluta reiciendis, sint quibusdam laborum maxime sed non accusantium?",
+                            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error delectus accusantium ut porro libero temporibus suscipit obcaecati molestias expedita repellendus sint, amet tenetur fugit facilis! Soluta iure cum fugiat vitae?"
+                        ]
+                    }
+                    buttons={
+                        [
+                            {url: "/trails", name: "Trilhas"},
+                            {url: "/courses", name: "Cursos"}
+                        ]
+                    }
                 />
             </Carousel>
         </div>
