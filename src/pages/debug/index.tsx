af59cb0e897f3591f489eacba3b6bf49220cb6eb
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2023-06-14 12:18:57 -0300

    Small layout changes

--- src/pages/debug/index.tsx
+++ src/pages/debug/index.tsx
@@ -4,9 +4,9 @@ import style from "./debug.module.scss";
 export default function DesignSystemPage() {
 	return (
 		<div id={style["root"]}>
-			<div id={style["primary"]}>
-				<div id={style["texts"]}>
-					<div id={style["titles"]}>
+			<div className={style["primary"]}>
+				<div className={style["texts"]}>
+					<div className={style["titles"]}>
 						<h1>OLÁ, EU SOU UM H1</h1>
 						<h2>OLÁ, EU SOU UM H2</h2>
 						<h3>OLÁ, EU SOU UM H3</h3>
@@ -15,13 +15,13 @@ export default function DesignSystemPage() {
 						<h6>OLÁ, EU SOU UM H6</h6>
 						<p>OLÁ, EU SOU UM P</p>
 					</div>
-					<div id={style["links"]}>
+					<div className={style["links"]}>
 						<a>OLÁ, EU SOU UM LINK SEM HREF</a>
 						<a href="/">OLÁ, EU SOU UM LINK COM HREF</a>
-						<a href="/">OLÁ, EU SOU UM BOTÃO LINK</a>
+						<a href="/" role="button">OLÁ, EU SOU UM BOTÃO LINK</a>
 					</div>
 				</div>
-				<fieldset id={style["forms"]}>
+				<fieldset className={style["forms"]}>
 					<input type="text" placeholder="OLÁ, EU SOU UM INPUT DE TEXTO" />
 					<input type="text" placeholder="OLÁ, EU SOU UM INPUT DE SENHA" />
 					<input type="number" placeholder="OLÁ, EU SOU UM INPUT NUMERICO" />
@@ -29,9 +29,9 @@ export default function DesignSystemPage() {
 					<input type="submit" value="OLÁ, EU SOU UM INPUT SUBMIT" />
 				</fieldset>
 			</div>
-			<div id={style["secondary"]}>
-				<div id={style["texts"]}>
-					<div id={style["titles"]}>
+			<div className={style["secondary"]}>
+				<div className={style["texts"]}>
+					<div className={style["titles"]}>
 						<h1>OLÁ, EU SOU UM H1</h1>
 						<h2>OLÁ, EU SOU UM H2</h2>
 						<h3>OLÁ, EU SOU UM H3</h3>
@@ -40,13 +40,13 @@ export default function DesignSystemPage() {
 						<h6>OLÁ, EU SOU UM H6</h6>
 						<p>OLÁ, EU SOU UM P</p>
 					</div>
-					<div id={style["links"]}>
+					<div className={style["links"]}>
 						<a>OLÁ, EU SOU UM LINK SEM HREF</a>
 						<a href="/">OLÁ, EU SOU UM LINK COM HREF</a>
 						<a href="/">OLÁ, EU SOU UM BOTÃO LINK</a>
 					</div>
 				</div>
-				<fieldset id={style["forms"]}>
+				<fieldset className={style["forms"]}>
 					<input type="text" placeholder="OLÁ, EU SOU UM INPUT DE TEXTO" />
 					<input type="text" placeholder="OLÁ, EU SOU UM INPUT DE SENHA" />
 					<input type="number" placeholder="OLÁ, EU SOU UM INPUT NUMERICO" />
